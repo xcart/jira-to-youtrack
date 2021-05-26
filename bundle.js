55e8969788653a9d336d387ed105e4fe107332eb
Eugene Dementjev <daemos@x-cart.com>
2021-05-26 14:15:07 +0400

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -1,9 +1,54 @@
 (function() {
     var JiraYoutrack = {
-      version: "0.0.1",
+      version: "0.0.2",
       renderUi: renderUi,
       token: null
     }
+    
+    var stylesheet = `
+    .css-button, .css-button:visited, .css-button:disabled, .css-button[disabled] {
+        -webkit-box-align: baseline;
+        align-items: baseline;
+        border-width: 0px;
+        border-radius: 3px;
+        box-sizing: border-box;
+        display: inline-flex;
+        font-size: inherit;
+        font-style: normal;
+        font-family: inherit;
+        font-weight: 500;
+        max-width: 100%;
+        position: relative;
+        text-align: center;
+        text-decoration: none;
+        transition: background 0.1s ease-out 0s, box-shadow 0.15s cubic-bezier(0.47, 0.03, 0.49, 1.38) 0s;
+        white-space: nowrap;
+        background: rgba(9, 30, 66, 0.04);
+        cursor: pointer;
+        height: 2.28571em;
+        line-height: 2.28571em;
+        padding: 0px 10px;
+        vertical-align: middle;
+        width: auto;
+        -webkit-box-pack: center;
+        justify-content: center;
+        outline: none;
+        margin: 0px;
+        color: rgb(66, 82, 110) !important;
+    }
+    
+    .css-button:hover {
+        text-decoration: inherit;
+        transition: background 0s ease-out 0s, box-shadow 0.15s cubic-bezier(0.47, 0.03, 0.49, 1.38) 0s;
+        background: rgba(9, 30, 66, 0.08);
+    }
+    
+    .css-button:active, .css-button:focus {
+        transition: background 0s ease-out 0s, box-shadow 0s cubic-bezier(0.47, 0.03, 0.49, 1.38) 0s;
+        background: rgba(179, 212, 255, 0.6);
+        color: rgb(0, 82, 204) !important;
+    }
+    `
     
     var event = new CustomEvent('jiraYoutrack.loaded', { detail: JiraYoutrack });
     document.dispatchEvent(event);
@@ -11,15 +56,32 @@
     function renderUi(options) {
         JiraYoutrack.token = options.token;
         JiraYoutrack.fetch = options.fetch;
-        var wrapper = document.createElement("td");
-        wrapper.style.paddingLeft = '30px';
-        var btn = document.createElement("input");
-        btn.value = "Create YouTrack Ticket";
-        btn.type = 'button';
-        btn.classList.add('submit');
+        addStylesheet();
+        findButtonPlacement().append(renderButton());
+    }
+    
+    function renderButton() {
+        var wrapper = document.createElement("div");
+        wrapper.setAttribute("role", "presentation");
+        var span = document.createElement("span");
+        var btn = document.createElement("button");
+        btn.type = "button";
+        btn.classList.add('css-button');
         btn.onclick = onClick;
-        wrapper.append(btn);
-        findButtonPlacement().append(wrapper);
+        var label = document.createElement("span");
+        label.textContent = "Copy to YT";
+        
+        btn.append(label);
+        span.append(btn);
+        wrapper.append(span);
+        
+        return wrapper;
+    }
+    
+    function addStylesheet() {
+        var style = document.createElement("style");
+        style.innerHTML = stylesheet;
+        document.getElementsByTagName("head")[0].appendChild(s);   
     }
 
     async function onClick(event) {
@@ -31,7 +93,7 @@
     }
 
     function findButtonPlacement() {
-        return document.querySelector('input[value="Update"]').closest('tr');
+        return document.querySelector('div[data-test-id="issue.views.issue-details.issue-layout.left-most-column"] button[aria-label="Attach"]').closest('div[role="presentation"]').parentElement;
     }
 
     async function gatherInfoObject() {
